e77b55bdf88ad2300d938c677761015f29a3740f
lifei <1968337133@qq.com>
2020-11-04 10:25:54 +0800

    添加了test方法

--- git-text/src/com/itheima/git/FirstClass.java
+++ git-text/src/com/itheima/git/FirstClass.java
@@ -10,4 +10,9 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
     }
+
+    public void test(){
+        int i = 0;
+        System.out.println(i);
+    }
 }
