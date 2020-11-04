91f2eefbe6bcd7327ee304bbe8b337105573f454
lifei <1968337133@qq.com>
2020-11-04 10:14:21 +0800

    把本地工程添加到git

---
+++ git-text/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,13 @@
+package com.itheima.git;
+
+/**
+ * Description:
+ * User: 一只小菜鸟
+ * Date:2020-11-04
+ * Time:10:04
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+    }
+}
